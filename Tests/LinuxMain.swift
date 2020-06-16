71199c811aed2fb3e0ff9a7f2f1da897d14eac97
janlionly <janlionly@gmail.com>
2020-06-16 16:10:06 +0800

    feat: support for SwiftPM

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swiftScan_1Tests
+
+var tests = [XCTestCaseEntry]()
+tests += swiftScan_1Tests.allTests()
+XCTMain(tests)
---
+++ Tests/swiftScan-1Tests/swiftScan_1Tests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import swiftScan_1
+
+final class swiftScan_1Tests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swiftScan_1().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
